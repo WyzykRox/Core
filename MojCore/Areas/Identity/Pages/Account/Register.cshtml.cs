a985a557e86b6081f9c7a110ebc10fb85779d67d
Wojciech Wyżywniak <wyzykrox@gmail.com>
2019-03-06 15:38:03 +0100

    Dodanie strony z komunikatem osprawdzemniu maila w celu potwierdzenia go.

--- MojCore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MojCore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,7 +80,7 @@ namespace MojCore.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Identity/Account/ConfirmMailToLogin");
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser()
